9ec84e5d9da1ad1cc31585a2550633afe005258e
Paul Rozhkin <rozhkin@protei.ru>
2020-03-13 13:30:37 +0300

    App create.

---
+++ src/main/java/com/itmo/goblinslayersystemserver/GoblinSlayerSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.goblinslayersystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblinSlayerSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoblinSlayerSystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/goblinslayersystemserver/GoblinSlayerSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.goblinslayersystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoblinSlayerSystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
