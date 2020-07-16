77dbab7399a9df43919479515c56e8ed31f0850b
Paul Rozhkin <blackiiifox@gmail.com>
2020-07-16 19:38:33 +0300

    update schema.database for 0.2.0, refactoring ranks

--- src/main/java/com/itmo/goblinslayersystemserver/models/enums/AdventurerRank.java
+++ src/main/java/com/itmo/goblinslayersystemserver/models/enums/AdventurerRank.java
@@ -1,14 +1,26 @@
 package com.itmo.goblinslayersystemserver.models.enums;
 
+import lombok.Getter;
+
 public enum AdventurerRank {
-     Platinum,    //10000
-     Gold,        //5000
-     Silver,      //1000
-     Bronze,      //500
-     Ruby,        //200
-     Emerald,     //100
-     Sapphire,    //50
-     Steel,       //10
-     Obsidian,    //5
-     Porcelain    //0
+     Platinum(10000),
+     Gold(5000),
+     Silver(1000),
+     Bronze(500),
+     Ruby(200),
+     Emerald(100),
+     Sapphire(50),
+     Steel(10),
+     Obsidian(5),
+     Porcelain(0);
+
+     /**
+      * Требуемое количество опыта для ранга (столбец adventurer_experience в БД)
+      */
+     @Getter
+     private final Integer experienceRequired;
+
+     private AdventurerRank(Integer experienceRequired) {
+          this.experienceRequired = experienceRequired;
+     }
 }
