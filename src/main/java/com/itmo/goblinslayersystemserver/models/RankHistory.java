a1f9aa57d75d885eebeba5d58a1fa3b15c47ab3a
Paul Rozhkin <blackiiifox@gmail.com>
2020-09-13 11:45:26 +0300

    add table for rank history

---
+++ src/main/java/com/itmo/goblinslayersystemserver/models/RankHistory.java
@@ -0,0 +1,48 @@
+package com.itmo.goblinslayersystemserver.models;
+
+import com.itmo.goblinslayersystemserver.models.enums.AdventurerRank;
+import com.itmo.goblinslayersystemserver.models.enums.RankHistoryType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "rank_history")
+@Data
+public class RankHistory extends BaseEntity {
+    /**
+     * ID авантюриста
+     **/
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "adventurer")
+    private User adventurer;
+
+    /**
+     * Старый ранг (с которого изменили)
+     **/
+    @Column(name="old_rank")
+    @Enumerated(EnumType.STRING)
+    private AdventurerRank oldRank;
+
+    /**
+     * Новый ранг (на который изменили)
+     **/
+    @Column(name="new_rank")
+    @Enumerated(EnumType.STRING)
+    private AdventurerRank newRank;
+
+    /**
+     * Тип повышения ранг
+     **/
+    @Column(name="type")
+    @Enumerated(EnumType.STRING)
+    private RankHistoryType type;
+
+    /**
+     * Причина изменения ранга (для типа Distributor)
+     */
+    @Column(name = "reason")
+    private String reason;
+}
---
+++ src/main/java/com/itmo/goblinslayersystemserver/models/enums/RankHistoryType.java
@@ -0,0 +1,13 @@
+package com.itmo.goblinslayersystemserver.models.enums;
+
+public enum RankHistoryType {
+    /**
+     * Ранг был распределен в автоматическом режиме при завершении контракта.
+     */
+    Auto,
+
+    /**
+     * Ранг был распределен в ручном режиме распределителем рангов.
+     */
+    Distributor
+}
