899baf8812437bc0de5e1d7979933bcbd0915169
Paul Rozhkin <blackiiifox@gmail.com>
2020-10-07 22:36:49 +0300

    add 403 status code for jwt error

--- src/main/java/com/itmo/goblinslayersystemserver/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/itmo/goblinslayersystemserver/security/jwt/JwtTokenFilter.java
@@ -2,7 +2,10 @@ package com.itmo.goblinslayersystemserver.security.jwt;
 
 import com.itmo.goblinslayersystemserver.models.User;
 import com.itmo.goblinslayersystemserver.services.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.itmo.goblinslayersystemserver.services.implementation.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -12,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -22,6 +26,7 @@ public class JwtTokenFilter extends GenericFilterBean {
 
     private IUserService userService;
     private JwtTokenProvider jwtTokenProvider;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public JwtTokenFilter(JwtTokenProvider jwtTokenProvider, IUserService userService) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -32,21 +37,26 @@ public class JwtTokenFilter extends GenericFilterBean {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
             throws IOException, ServletException {
 
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
+        try {
+            String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
 
-            if (auth != null) {
-                SecurityContextHolder.getContext().setAuthentication(auth);
-
-                User user = userService.get(auth.getName());
-                if (user.isBlocked()) {
-                    throw new JwtAuthenticationException("User is blocked.");
+                if (auth != null) {
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                    User user = userService.get(auth.getName());
+                    if (user.isBlocked()) {
+                        logger.info(String.format("User with id %d try to access to server, but he is blocked", user.getId()));
+                        throw new JwtAuthenticationException("User is blocked.");
+                    }
                 }
             }
-        }
 
-        filterChain.doFilter(req, res);
+            filterChain.doFilter(req, res);
+        } catch (JwtAuthenticationException e) {
+            logger.error("User with try to access to server, but error", e);
+            ((HttpServletResponse) res).sendError(HttpStatus.FORBIDDEN.value(), "Authorization shall be provided");
+        }
     }
 
 }
